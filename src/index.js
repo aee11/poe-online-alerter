8100510d26c762920d46ed0f8271dc870dee5dc3
Alexander Elís <alex2789@gmail.com>
2015-08-10 04:55:07 +0000

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+#! /usr/bin/env node
+
 import request from 'request';
 import yargs from 'yargs';
 import notifier from 'node-notifier';
+import moment from 'moment';
 
 const argv = yargs
   .usage('Usage: $0 ( --charName [name] | --accountName [name] ) --rate [minutes] --timeout [minutes]')
@@ -12,6 +15,10 @@ const argv = yargs
   .string('charName')
   .describe('accountName', 'The name of the account you want to watch.')
   .string('accountName')
+  .boolean('log')
+  .describe('log', 'Enables more detailed logging.')
+  .string('reason')
+  .describe('Reason for watching to remind you later.')
   .help('help')
   .showHelpOnFail(false, 'Specify --help for available options')
   .argv;
@@ -20,7 +27,9 @@ let {
   charName,
   accountName,
   rate,
-  timeout
+  timeout,
+  log,
+  reason
 } = argv;
 
 let nameType;
@@ -38,43 +47,69 @@ if (accountName) {
 }
 
 const globalTimeout = setTimeout(() => {
-  console.log(`Timeout expired: ${name} has not been online for the last `)
+  console.log(`Timeout expired: ${name} has not been online for the last `);
+  process.exit();
 }, timeout*60*1000);
+
+let firstRequest = true;
 
 function makeRequest() {
   const requestURL = `http://api.exiletools.com/ladder?league=allActive&${nameType}=${name}`;
-  console.log('Making request: ', requestURL);
   request(requestURL, (err, res, body) => {
     if (!err && res.statusCode == 200) {
       if (body.indexOf('ERROR / WARNING') == 0) {
         console.error(`Could not find ${nameType} ${name} on ladder.`);
         process.exit();
       }
-      let message;
       const characters = JSON.parse(body);
-      console.log(characters);
+
+      if (firstRequest && nameType === 'accountName') {
+        firstRequest = false;
+        const lastOnline = Object.keys(characters)
+          .map((character) => characters[character].lastOnline)
+          .reduce((acc, curr) => {
+            if (acc > curr) {
+              return acc;
+            } else {
+              return curr;
+            }
+          });
+        console.log(`User last seen online at ${moment(Number(lastOnline)*1000).format()}, ${moment(Number(lastOnline)*1000).fromNow()} (ladder only).`);
+      }
+
+      let message;
+      let shouldRetryNow = false;
       const isOnline = Object.keys(characters).some((characterKey) => {
         const character = characters[characterKey];
+
         if (character.online === '1') {
+          clearTimeout(globalTimeout);
           message = `${character.accountName} is online on character ${character.charName}.`;
-          console.log(message);
+          console.log(`[${moment().format('HH:mm:ss')}] - ${message}`);
           return true;
         } else if (nameType === 'charName') { // Widen search to accountName instead of charName
           nameType = 'accountName';
           name = character.accountName;
-          makeRequest();
-          return true;
+          shouldRetryNow = true;
         }
+        return false;
       });
-      if (!isOnline) {
+      if (shouldRetryNow) {
+        makeRequest();
+      } else if (!isOnline) {
+        if (log) {
+          console.log(`[${moment().format('HH:mm:ss')}] - ${name} is not online.`);
+        }
         setTimeout(makeRequest, rate*60*1000);
       } else {
+        if (reason) {
+          message += `\n${reason}`;
+        }
         notifier.notify({
-          'title': 'POE Alerter!',
+          'title': 'PoE Online Alert!',
           message,
           sound: true
         });
-        process.exit();
       }
     }
   });
